0e26a618b6984db99e35ad6b3f1f6258dbb19308
Szabson123 <szymonzaba18@gmail.com>
2024-07-31 10:09:41 +0200

    added changing months visualization under dates

--- src/app/plan/plan.component.ts
+++ src/app/plan/plan.component.ts
@@ -1,9 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import dayjs, { Dayjs } from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+import 'dayjs/locale/pl';
 import { PlanService, Event } from '../service/plan.service';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.locale('pl');
 
 @Component({
   selector: 'app-plan',
@@ -16,19 +23,22 @@ import { RouterModule } from '@angular/router';
 export class PlanComponent implements OnInit {
   events: Event[] = [];
   dates: Dayjs[] = [];
+  currentMonth: Dayjs;
 
-  constructor(private planService: PlanService) {}
+  constructor(private planService: PlanService) {
+    this.currentMonth = dayjs().tz('Europe/Warsaw').startOf('month');
+  }
 
   ngOnInit() {
     this.planService.getEvents().subscribe((data: Event[]) => {
       this.events = data;
+      this.generateDatesForCurrentMonth();
     });
-    this.generateDatesForCurrentMonth();
   }
 
   generateDatesForCurrentMonth() {
-    const startOfMonth = dayjs().startOf('month');
-    const endOfMonth = dayjs().endOf('month');
+    const startOfMonth = this.currentMonth.clone().startOf('month');
+    const endOfMonth = this.currentMonth.clone().endOf('month');
     const dates: Dayjs[] = [];
 
     let currentDate = startOfMonth;
@@ -39,5 +49,19 @@ export class PlanComponent implements OnInit {
     }
 
     this.dates = dates;
+  }
+
+  getEventsForDate(date: Dayjs): Event[] {
+    return this.events.filter(event => dayjs(event.date).isSame(date, 'day'));
+  }
+
+  previousMonth() {
+    this.currentMonth = this.currentMonth.subtract(1, 'month');
+    this.generateDatesForCurrentMonth();
+  }
+
+  nextMonth() {
+    this.currentMonth = this.currentMonth.add(1, 'month');
+    this.generateDatesForCurrentMonth();
   }
 }
